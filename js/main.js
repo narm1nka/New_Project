d78f02a7f08ff1f2046ecee11fa43d51cf7a8a78
Narmin Musayeva <115133058+narm1nka@users.noreply.github.com>
2023-01-06 09:41:28 -0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -47,3 +47,28 @@ $(document).on("scroll", function() {
      }, 500);
   });
 })();
+
+
+
+
+const list = document.querySelectorAll('.list')
+const itemBox = document.querySelectorAll('.item-box')
+
+list.forEach((elem)=>{
+    elem.addEventListener('click',function(){
+        list.forEach((elem)=>{
+            elem.classList.remove('active');
+        });
+        this.classList.add('active');
+
+        let value = this.getAttribute('data-filter');
+        itemBox.forEach(element=>{
+            element.classList.add('hide');
+            if(element.getAttribute('data-item') == value || value == "all"){
+                element.classList.remove('hide');
+            }
+        })
+
+    })
+
+})
